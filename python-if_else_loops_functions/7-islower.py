09249a1f187666111274c96fa5821d650dccf698
usanas7 <c.usanase@alustudent.com>
2024-02-25 09:02:40 -0800

    python2

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if i < j:
-            if str(i) + str(j) == str(89):
-                print("{}".format(str(i) + str(j)))
-            else:
-                print("{}".format(str(i) + str(j)), end=", ")
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
