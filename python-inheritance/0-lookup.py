a434834eefacccf6a37317bfe5abede73ac15db3
usanas7 <c.usanase@alustudent.com>
2024-03-14 15:02:04 -0700

    imheritance

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
+""" no module use """
+
+
 def lookup(obj):
-        """Returns a list of available attributes and methods of an object."""
-            return dir(obj)
-
-        # Test cases
-        class MyClass1(object):
-                pass
-
-            class MyClass2(object):
-                    my_attr1 = 3
-                        def my_meth(self):
-                                    pass
-
-                                print(lookup(MyClass1))
-                                print(lookup(MyClass2))
-                                print(lookup(int))
-
+    """ function outputs objects of class """
+    return dir(obj)
