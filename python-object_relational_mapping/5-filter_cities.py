226e08b0f57eef132c5bbb834d971db78997a3f5
usanas7 <c.usanase@alustudent.com>
2024-04-12 19:01:40 -0700

    mapping

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,24 @@
-
 #!/usr/bin/python3
-"""
-Lists all cities of a particular state
-"""
-import MySQLdb
+""" no injections this time! """
 import sys
+import MySQLdb
 
-
-if __name__ == '__main__':
-    con = MySQLdb.connect(db=sys.argv[3], user=sys.argv[1], passwd=sys.argv[2])
-    with con.cursor() as cur:
-        """Used context manager to automatically close the cursor object"""
-        cur.execute('''SELECT cities.name FROM cities JOIN states ON
-                    states.id=cities.state_id
-                    WHERE states.name = %s;''', (sys.argv[4], ))
-        print(", ".join([row[0] for row in cur.fetchall()]))
-    con.close()
-
+if __name__ == "__main__":
+    with MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            host='localhost',
+            port=3306,
+    )as conn:
+        cur = conn.cursor()
+        query = """ SELECT cities.name
+                    FROM states
+                    INNER JOIN cities ON states.id = cities.state_id
+                    WHERE states.name = '{:s}'
+                    ORDER BY cities.id ASC
+                """
+        cur.execute(query.format(sys.argv[4]))
+        cities = cur.fetchall()
+        print(", ".join([city[0] for city in cities]))
+        cur.close()
